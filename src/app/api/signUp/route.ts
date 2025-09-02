dde53a6ef64bae23b3aefe4e391e5e7c8a6e6e3e
Rudra2637 <singhrudra2637@gmail.com>
2025-09-02 18:28:14 +0530

    Sign Up Done

--- src/app/api/signUp/route.ts
+++ src/app/api/signUp/route.ts
@@ -2,13 +2,71 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import bcrypt from "bcryptjs";
 import { sendVerificationEmail } from "@/helper/sendVerificationEmail";
+import VerificationEmail from "../../../../emails/VerificationEmail";
 
 
 
 export async function POST(request:Request){
     await dbConnect()
     try {
-        
+        const {userName,email,password} = await request.json()
+        const userExist = await UserModel.findOne({
+            userName,
+            isVerified:true
+        })
+        if(userExist){
+            return Response.json({
+                success:false,
+                message:"User Name already Exists"
+            },{status:500})
+        }
+        const emailExist = await UserModel.findOne({email})
+        const verifyCode = Math.floor(100000 + Math.random() * 90000).toString()
+        if(emailExist){
+            if(emailExist.isVerified){
+                return Response.json({
+                    success:false,
+                    message:"User Exist with this email"
+                },{status:400})
+            }
+            else{
+                const hashPassword = await bcrypt.hash(password,10)
+                emailExist.password = hashPassword
+                emailExist.verifyCode = verifyCode
+                emailExist.verifyCodeExpiry = new Date(Date.now() + 3600000)
+                await emailExist.save()
+            }
+        }
+        else{
+            const hashPassword = await bcrypt.hash(password,10)
+            const codeExpiry = new Date()
+            codeExpiry.setHours(codeExpiry.getHours() + 1)
+            const newUser = await new UserModel({
+                    username:userName,
+                    email,
+                    password:hashPassword,
+                    verifyCode,
+                    verifyCodeExpiry:codeExpiry,
+                    isVerified:false,
+                    isAcceptingMessage:true,
+                    message:[]
+            })
+            await newUser.save()
+        }
+        const emailResponse = await sendVerificationEmail(
+            email,userName,verifyCode
+        )
+        if(!emailResponse.success){
+            return Response.json({
+                success:false,
+                message:emailResponse.message
+            },{status:500})
+        }
+        return Response.json({
+            success:true,
+            message:"User resgistered Successfully,Please Verify the email"
+        },
+        {status:201})
 
     } catch (error) {
         console.error("Error in registering user ",error)
