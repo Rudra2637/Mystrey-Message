ffdbf8ba1d1c3f695c23346b931868a3c59a53f4
Rudra2637 <singhrudra2637@gmail.com>
2025-09-03 23:51:34 +0530

    Login Done

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -40,15 +40,6 @@ export const authOptions : NextAuthOptions = {
         })
     ],
     callbacks:{
-        async session({ session, token }) {
-            if(token){
-                session.user._id = token._id
-                session.user.isVerified = token.isVerified
-                session.user.isAcceptingMessages = token.isAcceptingMessages
-                session.user.username = token.username
-            }
-            return session
-        },
         async jwt({ token, user }) {
             if(user){
                 token._id = user._id?.toString()
@@ -57,6 +48,15 @@ export const authOptions : NextAuthOptions = {
                 token.username = user.username
             }
             return token
+        },
+        async session({ session, token }) {
+            if(token){
+                session.user._id = token._id
+                session.user.isVerified = token.isVerified
+                session.user.isAcceptingMessages = token.isAcceptingMessages
+                session.user.username = token.username
+            }
+            return session
         }
     },
     pages:{
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,31 @@
 import {NextRequest ,NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+
+import { getToken } from 'next-auth/jwt'    
  
+
+export {default} from "next-auth/middleware"              //exporting the default middleware from next-auth
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+export async function middleware(request: NextRequest) {           //middleware function that will run before any request is made to the specified paths
+
+  const token = await getToken({req:request})
+  const url = request.nextUrl
+  if(token &&                                      
+    (
+      url.pathname.startsWith('/sign-in') || 
+      url.pathname.startsWith('/sign-up') ||
+      url.pathname.startsWith('/verify')  ||
+      url.pathname.startsWith('/') 
+    )
+  )  return NextResponse.redirect(new URL('/home', request.url))
+  
+  if(!token && url.pathname.startsWith('/dashboard')){
+    return NextResponse.redirect(new URL('/sign-in',request.url))
+  }
+  return NextResponse.next()
 }
  
 // See "Matching Paths" below to learn more
-export const config = {
+export const config = {                       //config object to specify the paths where we want to use this middleware
   matcher: [
     '/sign-in',
     '/sign-up',
